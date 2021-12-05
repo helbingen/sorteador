0f616141857852ec09ac87211406b6e60ab0c891
Daniel Helbingen <48421336+helbingen@users.noreply.github.com>
2021-12-05 00:44:15 -0300

    Add files via upload

---
+++ sorteador.js
@@ -0,0 +1,26 @@
+var sorteador = function () {
+    var x1 = Math.ceil(Math.random() * 60);
+    var x2 = Math.ceil(Math.random() * 60);
+    var x3 = Math.ceil(Math.random() * 60);
+    var x4 = Math.ceil(Math.random() * 60);
+    var x5 = Math.ceil(Math.random() * 60);
+    var x6 = Math.ceil(Math.random() * 60);
+    var n1 = document.getElementById("n1");
+    var n2 = document.getElementById("n2");
+    var n3 = document.getElementById("n3");
+    var n4 = document.getElementById("n4");
+    var n5 = document.getElementById("n5");
+    var n6 = document.getElementById("n6");
+    n1.innerHTML = x1;
+    n2.innerHTML = x2;
+    n3.innerHTML = x3;
+    n4.innerHTML = x4;
+    n5.innerHTML = x5;
+    n6.innerHTML = x6;
+
+    // for(var i=0; i<6; i++){
+    //     var x = Math.ceil(Math.random()*60);
+    //     var n = document.getElementById("n",)
+    // }
+
+}
